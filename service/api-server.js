08984ecfddad4823528f192f8a2373665329032c
Roie Schwaber-Cohen <roie.cohen@gmail.com>
2021-11-16 12:02:29 -0800

    serverless express

---
+++ service/api-server.js
@@ -0,0 +1,60 @@
+require('dotenv').config({ path: '../.env.local' });
+
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const jwt = require('express-jwt');
+const jwksRsa = require('jwks-rsa');
+
+const app = express();
+const router = express.Router();
+const port = process.env.API_PORT || 3001;
+const baseUrl = process.env.AUTH0_BASE_URL;
+const issuerBaseUrl = process.env.AUTH0_ISSUER_BASE_URL;
+const audience = process.env.AUTH0_AUDIENCE;
+
+const isNetlify = process.env.NETLIFY || process.env.REACT_APP_NETLIFY;
+const routerBasePath = isNetlify ? '/.netlify/functions/api-server' : '/';
+
+
+if (!baseUrl || !issuerBaseUrl) {
+    throw new Error('Please make sure that the file .env.local is in place and populated');
+}
+
+if (!audience) {
+    console.log('AUTH0_AUDIENCE not set in .env.local. Shutting down API server.');
+    process.exit(1);
+}
+
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors({ origin: baseUrl }));
+app.use(routerBasePath, router);
+const checkJwt = jwt({
+    secret: jwksRsa.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: `${issuerBaseUrl}/.well-known/jwks.json`
+    }),
+    audience: audience,
+    issuer: `${issuerBaseUrl}/`,
+    algorithms: ['RS256']
+});
+
+app.get('/api/shows', checkJwt, (req, res) => {
+
+    res.send({
+        msg: 'Your access token was successfully validated!'
+    });
+});
+
+if (isNetlify) {
+    const serverless = require("serverless-http");
+    exports.handler = serverless(app);
+} else {
+    const server = app.listen(port, () => console.log(`API Server listening on port ${port}`));
+    process.on('SIGINT', () => server.close());
+}
+
