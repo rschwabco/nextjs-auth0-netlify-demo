acc9b3696f15bcc0749b74755fc959796a1f9df1
Roie Schwaber-Cohen <roie.cohen@gmail.com>
2021-11-16 13:36:19 -0800

    whoops

--- pages/api/shows.js
+++ pages/api/shows.js
@@ -9,7 +9,7 @@ export default withApiAuthRequired(async function shows(req, res) {
         const apiOrigin = `https://objective-mclean-c4ee52.netlify.app/.netlify/functions/api-server`
         const { accessToken } = await getAccessToken(req, res);
         const apiPort = process.env.API_PORT || 3001;
-        const response = await fetch(`${apiOrigin}}/api/shows`, {
+        const response = await fetch(`${apiOrigin}/api/shows`, {
             headers: {
                 Authorization: `Bearer ${accessToken}`
             }
